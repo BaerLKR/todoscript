f51f4f6d0de2f9a8a8b7d4887e2eb5af3759c3b4
BaerLKR <83340886+BaerLKR@users.noreply.github.com>
2022-12-15 22:01:49 +0100

    license update

--- todo.sh
+++ todo.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-#The GPLv3 License (GPLv3)
 
-#Copyright (c) 2022 Lovis Rentsch
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+ 
+# Copyright (C) 2022 Lovis Rentsch <root@lovirent.eu>
 
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+ 
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 
 #shellcolorcodes von stackoverflow
